31f106ce2155156de858e55eba06d97d1401842d
Ken Robertson <ken@invalidlogic.com>
2010-10-24 14:12:50 -0700

    Some more global settings, cleanup, commenting

--- ken/global.el
+++ ken/global.el
@@ -1,20 +1,46 @@
 (require 'cl)
+
+;; My preferred font
+(set-face-font 'default "-apple-Mensch-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
+
+;; Setup ido mode
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-(setq tags-file-name ".TAGS")
+;; Tabs/indentation
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
-(setq mac-emulate-three-button-mouse nil)
-(setq cua-highlight-region-shift-only t)
+
+;; Some coding preferences
+(setq tags-file-name ".TAGS")
 (prefer-coding-system 'utf-8)
 (setq vc-handled-backends nil)
-(cua-mode nil)
 (setq gist-view-gist t)
+
+;; Cut and paste mode
+(cua-mode nil)
+(setq cua-highlight-region-shift-only t)
+
+;; Don't emulate 3 button mouse
+(setq mac-emulate-three-button-mouse nil)
+
+;; Don't confirm non-existent files
+(setq confirm-nonexistent-file-or-buffer nil)
+
+;; Auto revert when switching branches
+(global-auto-revert-mode 1)
+
+;; Prevent splash/startup screen
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+;; No menu mode
 (menu-bar-mode -1)
 
-(set-face-font 'default "-apple-Mensch-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
+;; I use version control, don't annoy me with backup files everywhere
+(setq make-backup-files nil)
+(setq auto-save-default nil)
 
 ;; works in both aquamacs and carbon
 (when (functionp 'tool-bar-mode)
@@ -27,3 +53,17 @@
 ;; remove scroll bar
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
+
+;;; When selecting a file to visit, // will mean / and
+;;; ~ will mean $HOME regardless of preceding text.
+(setq file-name-shadow-tty-properties '(invisible t))
+(file-name-shadow-mode 1)
+
+;;show a marker in the left fringe for lines not in the buffer
+(setq default-indicate-empty-lines t)
+
+;; Set the frame's title. %b is the name of the buffer. %+ indicates the
+;; state of the buffer: * if modified, % if read only, or - otherwise.
+;; Two of them to emulate the mode line. %f for the file name
+;; (absolute path actually).
+(setq frame-title-format "Emacs: %b %+%+ %f")
